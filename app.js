b1c5a8f42d19f9f3195d5231ed61699f323b1859
Amari <adamsamari@icloud.com>
2021-09-18 19:08:04 -0400

    i

---
+++ app.js
@@ -0,0 +1,62 @@
+ let inp = document.getElementById('input');
+
+function add() {
+  console.log("+")
+}
+
+function subtract() {
+  console.log("-")
+}
+
+function multiply() {
+  console.log("*")
+}
+
+function divide() {
+  console.log("÷")
+}
+
+function operate () {
+  
+}
+
+function one () {
+  inp.value =  "1";
+  inp.innerHTML = "1 = " + "'" + inp.value + "'";
+}
+
+function two () {
+  inp.value =  "2";
+}
+
+function three () {
+  inp.value =  "3";
+}
+
+function four () {
+  inp.value =  "4";
+}
+
+function five () {
+  inp.value =  "5";
+}
+
+function six () {
+  inp.value =  "6";
+}
+
+function seven () {
+  inp.value =  "7";
+}
+
+function eight () {
+  inp.value =  "8";
+}
+
+function nine () {
+  inp.value =  "9";
+}
+
+function zero () {
+  inp.value =  "0";
+}
